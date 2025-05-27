40d55c732018f97ad484b25f49cff4f60b449f37
Thomas Walawgo <mail@fluxo.io>
2025-05-27 11:41:36 +0200

    start script, documentation, structuing and renaming

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
+#! /usr/bin/env node
 import fs from "fs";
 import path from "path";
-import { processFiles } from "./modules/validator.js";
+import { validateFiles } from "./modules/validator.js";
+import { compareFiles } from "./modules/comparator.js";
+
+const [,, command, ...args] = process.argv;
 
 const schemaDir = "schema";
 const importDir = "imports";
 const exportDir = "exports";
 
-[schemaDir, exportDir, importDir].forEach((dir) => {
-  if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir, { recursive: true });
-  }
+// Ensure directories exist
+[schemaDir, importDir, exportDir].forEach(dir => {
+  if (!fs.existsSync(dir)) fs.mkdirSync(dir, { recursive: true });
 });
 
-const schemaPath = path.join(schemaDir, "oBDS_v3.0.3.xsd");
-processFiles(schemaPath,importDir,exportDir);
+switch (command) {
+  case "validate":
+    const schemaPath = path.join(schemaDir, "oBDS_v3.0.3.xsd");
+    validateFiles(schemaPath, importDir, exportDir);
+    break;
+  case "compare":
+    // args[0] = templateXML, args[1] = compareXML
+    if (args.length < 2) {
+      console.error("Usage: node index.js compare <template.xml> <compare.xml>");
+      process.exit(1);
+    }
+    compareFiles(args[0], args[1]);
+    break;
+  default:
+    console.log("So muss das Skript genutzt werden:");
+    console.log("  gegen ein XML-Schema validieren:   node index.js validate");
+    console.log("  zwei XMLs vergleichen:             node index.js compare datei1.xml datei2.xml");
+    process.exit(1);
+}
--- modules/validator.js
+++ modules/validator.js
@@ -3,7 +3,7 @@ import fs from "fs";
 import path from "path";
 
 
-export async function processFiles(schemaPath, importDir, exportDir) {
+export async function validateFiles(schemaPath, importDir, exportDir) {
   const files = fs.readdirSync(importDir).filter(file => file.endsWith(".xml"));
 
   for (const file of files) {
